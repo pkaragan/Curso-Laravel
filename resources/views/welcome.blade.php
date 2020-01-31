c15db00f8b17ccc1b48679c08e6ab596a0183e36
pkaragan <frgmendoza@gmail.com>
2020-01-30 21:30:39 -0700

    Se agregaron los cambio de prueba

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -82,6 +82,7 @@
             <div class="content">
                 <div class="title m-b-md">
                     Laravel
+                    Mensaje de prueba
                 </div>
 
                 <div class="links">
