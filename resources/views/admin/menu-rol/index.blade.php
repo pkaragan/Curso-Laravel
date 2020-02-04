c5324c1e586a4c2867c05fbb3b891cd2e8f37848
pkaragan <frgmendoza@gmail.com>
2020-02-04 09:18:13 -0700

    terminado video 27 - menu dinamido

--- resources/views/admin/menu-rol/index.blade.php
+++ resources/views/admin/menu-rol/index.blade.php
@@ -11,7 +11,7 @@ Menú - Rol
 <div class="row">
     <div class="col-lg-12">
         @include('includes.mensaje')
-        <div class="card">
+        <div class="card card-success">
             <div class="card-header with-border">
                 <h3 class="card-title">Menús - Rol</h3>
             </div>
--- resources/views/theme/lte/header.blade.php
+++ resources/views/theme/lte/header.blade.php
@@ -1,14 +1,14 @@
-<nav class="main-header navbar navbar-expand navbar-white navbar-light">
+<nav class="main-header navbar navbar-expand navbar-lightblue navbar-light">
     <!-- Left navbar links -->
     <ul class="navbar-nav">
       <li class="nav-item">
         <a class="nav-link" data-widget="pushmenu" href="#"><i class="fas fa-bars"></i></a>
       </li>
       <li class="nav-item d-none d-sm-inline-block">
-        <a href="/" class="nav-link">Home</a>
+        <a href="/" class="nav-link text-bold">Home</a>
       </li>
       <li class="nav-item d-none d-sm-inline-block">
-        <a href="#" class="nav-link">Contact</a>
+        <a href="#" class="nav-link text-bold">Contact</a>
       </li>
     </ul>
 
